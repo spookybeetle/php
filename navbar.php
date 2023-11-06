5910cf51bb30cfbec356e7975f82f8fe8bc6aa3e
spookybeetle <krw5520@psu.edu>
2023-11-05 20:15:23 -0500

    Setup site

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<?php
+echo'
+<ul id="nav">
+	<li class="nav-link">
+		<h1 data-name="home">Home</h1>
+	</li>
+	<li class="nav-link">
+		<h1 data-name="About">About</h1>
+	</li>
+	<li class="nav-link">
+		<h1 data-name="services">services</h1>
+	</li>
+	<li class="nav-link">
+		<h1 data-name="contact">contact</h1>
+	</li>
+</ul>
+
+<!-- ------------ -->
+<!-- Social links -->
+<aside class="_social-links">
+	<ul class="_links-list">
+		<li class="_social-link"><a href="https://twitter.com/ggsingla" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
+		<li class="_social-link"><a href="https://github.com/ggsingla" target="_blank"><i class="fa-brands fa-github"></i></a></li>
+		<li class="_social-link"><a href="mailto:ggsingla777@gmail.com" target="_blank"><i class="fa-solid fa-at"></i></a></li>
+	</ul>
+</aside>'
+?>
